7045aaedc17bf1c9c3e1a33f6c96bbc505c3c67c
Leonardo Pinzon <leonardo3103@gmail.com>
2018-11-26 23:41:56 -0400

    update triangle create

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -27,15 +27,12 @@ void CTriangle::display()
 
 void CTriangle::createTriangle(int x0, int y0, int x1, int y1, int x2, int y2) {
 	CLine *line1 = new CLine();
-	CLine *line2 = new CLine();
-	CLine *line3 = new CLine();
 
-		line1->createLine(x0, y0, x1, y1);
+	line1->createLine(x0, y0, x1+1, y1);
 
-		line1->createLine(x0+1, y0+1, x2, y2);
-	
+	line1->createLine(x0 + 1, y0 , x2, y2);
 
-		line1->createLine(x1 + 1, y1+1, x2-1, y2-1);
+	line1->createLine(x1 + 1, y1 , x2 - 1, y2 );
 
 
 }
